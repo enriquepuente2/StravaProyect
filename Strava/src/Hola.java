bfba1f1d120202830b9144b98db0d932a8730508
IsmaelAlcaide <IsmaelAlcaide@localhost>
2022-11-10 12:48:26 +0100

    sdj

--- Strava/src/Hola.java
+++ Strava/src/Hola.java
@@ -3,5 +3,6 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Adios de parte de kekp");
+		System.out.println("Buenas");
 	}
 }
